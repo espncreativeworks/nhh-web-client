c505cfc66ee52121c1b1bd19e6d3b181bc3d454b
Chris Montoro <cmonto1@hotmail.com>
2014-09-10 10:03:31 -0400

    Update copy

--- app/scripts/controllers/tourStopList.js
+++ app/scripts/controllers/tourStopList.js
@@ -12,8 +12,8 @@ angular.module('nhhApp')
     var desc = '' +
       'Nissan and ESPN invite fans to "Get to know the Heisman Winners" during a ' +
       'pre-game experience that celebrates college football\'s most outstanding players. ' +
-      'The Nissan Heisman House Tour is making stop at 10 marquee matchups this season ' +
-      'including every playoff championship game. ' +
+      'The Nissan Heisman House Tour is making stop at 10 marquee matchups this ' +
+      'season including its ultimate destination of Arlington for the College Football Playoff National Championship. ' +
       'Admission to the tour is free! Come join us for fun interactives to win prizes and ' +
       'the opportunity to meet former Heisman winners.';
 
@@ -31,7 +31,7 @@ angular.module('nhhApp')
         return a.stopNumber - b.stopNumber;
       });
       angular.forEach($scope.stops, function (stop){
-        // server TZ is UTC; force date to be displayed in local TZ equivalent 
+        // server TZ is UTC; force date to be displayed in local TZ equivalent
         stop.stopDate = $moment.tz($moment(stop.stopDate).endOf('day'), 'Europe/London').format('MMM D, YYYY');
         stop.summaryHtml = $sce.trustAsHtml(stop.summary);
         stop.map = {
